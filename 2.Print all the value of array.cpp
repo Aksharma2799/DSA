8a803e5bc47de21c14237e8fed3f0a29548aaa88
ASHISH VISHWAKARMA <75712767+Aksharma2799@users.noreply.github.com>
2023-02-15 16:19:21 +0530

    Array

--- 2.Print all the value of array.cpp
+++ 2.Print all the value of array.cpp
@@ -6,7 +6,7 @@ int main() {
   //array declare
 
   int arr[] = {1,3,4,5,6};
-//print all the value of array
+ //print all the value of array
   for(int i = 0; i<5; i++){
      cout << arr[i] << endl;
 
