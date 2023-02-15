ecc00355f305b4817f781831c89d52b7a37dbda7
ASHISH VISHWAKARMA <75712767+Aksharma2799@users.noreply.github.com>
2023-02-15 16:20:09 +0530

    Array

--- 3.Input Output in Array.cpp
+++ 3.Input Output in Array.cpp
@@ -3,9 +3,10 @@ using namespace std;
 
 int main() {
 
-//Taking input in array
+
 
 int arr[5];
+//Taking input in array
 cout<<"Enter the values of array" << endl;
 for(int i = 0; i<5; i++){
   cin >> arr[i];
